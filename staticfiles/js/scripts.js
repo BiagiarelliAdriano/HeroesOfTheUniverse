52bb3a715ab2d56daa96c973f4bbd2b618cf5b84
Adriano Biagiarelli <adrianobiagiarelli@gmail.com>
2024-11-26 10:40:47 +0000

    Enable serving of static files

---
+++ staticfiles/js/scripts.js
@@ -0,0 +1,5 @@
+// Function to show hidden buttons when the title button is clicked
+function showButtons() {
+    const hiddenButtons = document.getElementById('hidden-buttons');
+    hiddenButtons.style.display = 'flex'; // Show hidden buttons
+}
