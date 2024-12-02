667d6fce9c139526ed43154d6bc04b6c84f582ac
Adriano Biagiarelli <adrianobiagiarelli@gmail.com>
2024-12-02 11:53:30 +0000

    Adjust character sheet for larger screen sizes

--- universe/character_views.py
+++ universe/character_views.py
@@ -13,20 +13,7 @@ def create_or_edit_character(request, character_id=None):
         weapon_formset = WeaponFormSet(request.POST or None)
 
     if request.method == 'POST':
-        print("Form data:", request.POST)  # Debug: Check incoming POST data
-        if form.is_valid():
-            print("Character form is valid.")  # Debug: Character form is valid
-        else:
-            print("Character form errors:", form.errors)  # Debug: Print errors for CharacterForm
-
-        if weapon_formset.is_valid():
-            print("Weapon formset cleaned data:", weapon_formset.cleaned_data)
-        else:
-            print("Weapon formset errors:", weapon_formset.errors)
-
-
         if form.is_valid() and weapon_formset.is_valid():
-            print("Both forms are valid. Saving...")  # Debug: Proceeding to save data
             character = form.save(commit=False)
             character.user = request.user
             character.save()
@@ -40,9 +27,3 @@ def create_or_edit_character(request, character_id=None):
             return redirect('character_detail', character_id=character.id)
 
     return render(request, 'universe/character.html', {'form': form, 'weapon_formset': weapon_formset})
-
-
-
-def character_detail(request, character_id):
-    character = Character.objects.get(id=character_id)
-    return render(request, 'character_detail.html', {'character': character})
--- universe/models/weapon_model.py
+++ universe/models/weapon_model.py
@@ -6,7 +6,7 @@ class Weapon(models.Model):
     name = models.CharField(max_length=100)
     attack_bonus_save_dc = models.IntegerField(default=0)
     damage_type = models.CharField(max_length=100, blank=True, null=True)
-    weapon_notes = models.TextField(max_length=100, blank=True, null=True)
+    weapon_notes = models.TextField(max_length=500, blank=True, null=True)
 
     def __str__(self):
         return f"{self.name} ({self.character.name})"
