31420c1ede2cf4d21a3ad564299854f2a5c8c0dd
Adriano Biagiarelli <adrianobiagiarelli@gmail.com>
2024-12-07 09:50:57 +0000

    Add Comment model

--- homepage/models.py
+++ homepage/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+from universe.models import Character
+
 
 # Create your models here.
+class Comment(models.Model):
+    character = models.ForeignKey(Character, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
